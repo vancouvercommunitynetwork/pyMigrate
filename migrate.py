c4a3c07ac576a8e58a6059bed965551081eb5869
CodeBishop <sbishop@longdays.com>
2016-09-25 01:46:26 -0700

    Experimenting with ways of reading local and remote /etc/passwd and /etc/shadow files.

--- migrate.py
+++ migrate.py
@@ -1,10 +1,53 @@
 #!/usr/bin/env python
+#
+# TODO
+#
 
-	
-with open('/etc/passwd','r') as localPasswdFile:
-    lines = localPasswdFile.readlines()
 
-for line in lines:
-    print line.split(':')
-    
+import subprocess
+# ssh = subprocess.Popen(['ssh', 'pi@192.168.1.11', 'cat', '/etc/passwd'], stdout=subprocess.PIPE)
+
+
+# Converts a list of lines to a dictionary keyed on the first thing before the delimiter.
+def makeDictBySplitToFirstField(lines, delimiter):
+    dict = {}
+    for line in lines:
+        head = line.split(delimiter, 1)[0]
+        dict[head] = line
+    return dict
+
+
+def getRemoteUserData(target):
+    # Read the /etc/passwd file into a list of lines, then convert it to a dictionary keyed by username.
+    passwdFile = subprocess.Popen(['ssh', target, 'cat', '/etc/passwd'], stdout=subprocess.PIPE).stdout
+    passwdEntries = passwdFile.read().splitlines()
+    passwdDict = makeDictBySplitToFirstField(passwdEntries, ':')
+
+    # Read the /etc/shadow file into a list of lines, then convert it to a dictionary keyed by username.
+    shadowFile = subprocess.Popen(['ssh', target, 'sudo cat', '/etc/shadow'], stdout=subprocess.PIPE).stdout
+    shadowEntries = shadowFile.read().splitlines()
+    shadowDict = makeDictBySplitToFirstField(shadowEntries, ':')
+
+    return passwdDict, shadowDict
+
+
+def getLocalUserData():
+    # Read the /etc/passwd file into a list of lines.
+    with open('/etc/passwd', 'r') as localPasswdFile:
+        passwdEntries = localPasswdFile.read().splitlines()
+    passwdDict = makeDictBySplitToFirstField(passwdEntries, ':')
+
+    # Read the /etc/shadow file into a dictionary of lines keyed by username.
+    with open('/etc/shadow', 'r') as shadowPasswdFile:
+        shadowEntries = shadowPasswdFile.read().splitlines()
+    shadowDict = makeDictBySplitToFirstField(shadowEntries, ':')
+
+    return passwdDict, shadowDict
+
+
+srcPasswdDict, srcShadowDict = getLocalUserData()
+destPasswdDict, destShadowDict = getRemoteUserData('pi@192.168.1.11')
+print destPasswdDict['test3']
+# print getLocalUserData()
+
 
