c276c8e5ee1df9ced8d21fc06a71bd093f58183a
CodeBishop <srb@vcn.bc.ca>
2016-09-27 14:42:03 -0700

    Cleaned up the code, renamed some variables and methods.

--- migrate.py
+++ migrate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #
+#
 # TO DO
+# Propagate user deletions. If they're not on the source they should be deleted from the destination.
+# Document that passwords are the only user change this program updates.
 # Error Modes to Cover:
 #   No route to host: a remote machine can't be found on the network.
 #   No SSH pre-authorization for remote machine.
@@ -21,7 +24,8 @@ LOWEST_USER_ID = 1000       # User IDs below this are for system accounts.
 MOST_USERNAMES_TO_LIST = 5  # No message should dump more than this many usernames.
 
 
-class Account():
+# An object to represent the attributes of a linux user account.
+class Account:
     def __init__(self, passwdEntry, shadowEntry):
         # Pull the needed fields from the entries. The field ordering in passwd and shadow are:
         #    username:password:userID:groupID:gecos:homeDir:shell
@@ -87,13 +91,37 @@ def getNonSystemAccounts(usernameList, passwdDict, shadowDict):
 def sshAddRemoteUser(target, account):
     # Construct command.
     cmd = 'ssh -n ' + target + ' /usr/sbin/useradd -p "' + \
-          account.password + '" -g ' + account.gid
+          account.password + '" -u ' + account.uid + ' -g ' + account.gid
     if account.gecos != "":
         cmd += '" -c "' + account.gecos + '"'
     cmd += ' -M -s ' + account.shell + ' ' + account.username
     output = commands.getoutput(cmd)
     if output:
         print output
+
+
+def sshChangeUserPassword(target, username, newPassword):
+    # Construct command.
+    cmd = "ssh -n " + target + " /usr/sbin/usermod -p '" + newPassword + "' " + username
+
+    output = commands.getoutput(cmd)
+    if output:
+        print output
+
+
+def limitedUserListString(lulsUserList):
+    returnString = ""
+    # Construct a string listing users without being too long.
+    if len(lulsUserList) <= MOST_USERNAMES_TO_LIST:
+        for lulUsername in userList:
+            returnString += lulUsername + ' '
+    # If there's lots of missing users then just list the first few
+    else:
+        for lulUsername in lulsUserList[:MOST_USERNAMES_TO_LIST]:
+            print lulUsername,
+            returnString += "... and " + \
+                str(len(lulsUserList) - MOST_USERNAMES_TO_LIST) + " others."
+    return returnString
 
 
 # Settings that will later be taken as command-line arguments.
@@ -104,7 +132,6 @@ userListFile = 'list_of_users.txt'
 # Load user files from source and destination machines.
 srcPasswdDict, srcShadowDict = getLocalUserData()
 destPasswdDict, destShadowDict = getRemoteUserData(destAddress)
-# destPasswdDict, destShadowDict = getRemoteUserData()
 
 # Load list of users.
 with open(userListFile, 'r') as localPasswdFile:
@@ -114,28 +141,21 @@ with open(userListFile, 'r') as localPasswdFile:
 missingUsers, newUsers, changedUsers = [], [], []
 newUserCount, changedUserCount, unchangedUserCount = 0, 0, 0
 
-# Look for users that are missing from the source, new at the destination or
-# present at both but have account attributes that have changed.
+# Analyse users from the given text file of usernames.
 for username in userList:
-    # If a user is not found on the source machine then mark it as missing.
+    # If username not found at source machine add them to list of missing users.
     if username not in srcPasswdDict:
         missingUsers.append(username)
 
-    # If a user exists at the source but not the destination then mark it as a new user.
+    # If username found at source but not at destination then copy them over.
     if username in srcPasswdDict and username not in destPasswdDict:
         newUsers.append(username)
 
-    # If a user exists at both the source and the destination then check if it has changed.
+    # If username found at source and destination then check if password has changed.
     if username in srcPasswdDict and username in destPasswdDict:
-        if srcPasswdDict[username] != destPasswdDict[username]:
-            changedUsers.append(username)
-            # DEBUG:
-            print username + " has changed data."
-            print "Data at source:"
-            print srcPasswdDict[username]
-            print "Data at destination:"
-            print destPasswdDict[username]
-        elif srcShadowDict[username] != destShadowDict[username]:
+        srcPassword = srcShadowDict[username].split(':')[1]
+        destPassword = destShadowDict[username].split(':')[1]
+        if srcPassword != destPassword:
             changedUsers.append(username)
         else:
             unchangedUserCount += 1
@@ -152,23 +172,14 @@ if newUsers:
 if changedUsers:
     changedAccounts = getNonSystemAccounts(changedUsers, srcPasswdDict, srcShadowDict)
     for account in changedAccounts:
-        #TO DO: Add code for determining what the user has changed and mentioning it to stdout.
-        print "Updating user: " + account.username
-        #TO DO: Add code for ssh to update existing user at remote machine.
+        print "Updating password for user: " + account.username
+        sshChangeUserPassword(destAddress, account.username, account.password)
 
 # Warn of user names that were not found on the source machine.
 if missingUsers:
     print "WARNING: The following users were listed in \"" + userListFile + \
           "\" but could not be found on the source machine:",
-    # List all missing users if there's not too many.
-    if len(missingUsers) <= MOST_USERNAMES_TO_LIST:
-        for username in missingUsers:
-            print username,
-    # If there's lots of missing users then just list the first few
-    else:
-        for username in missingUsers[:MOST_USERNAMES_TO_LIST]:
-            print username,
-        print "... and " + str(len(missingUsers) - MOST_USERNAMES_TO_LIST) + " others.",
+
     print
 
 # Give a final accounting the user migration results.
