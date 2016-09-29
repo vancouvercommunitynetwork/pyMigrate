72cade0ed9f6f00736912e21fed41944750aa7cb
CodeBishop <srb@vcn.bc.ca>
2016-09-29 11:55:43 -0700

    Renamed getUserData() to getNonSystemUserData(), completed basic testing of it and switched over main() to use it instead of the older functions which have now been removed.

--- migrate.py
+++ migrate.py
@@ -10,6 +10,7 @@
 # Remove the functions that return dictionaries of entries after switching over to getUserData().
 # Add the feature of an unlistedUsersGetDeleted flag and default to false.
 # Experiment with list operations to find concision. In particular, doomedUsers could be formed by list subtraction.
+# Test performance of list building. A for-loop over migrantUsers may be more efficient than using list comprehensions.
 # Error Modes to Cover:
 #   No route to host: a remote machine can't be found on the network.
 #   No SSH pre-authorization for remote machine.
@@ -48,15 +49,6 @@ EXIT_CODE_FAILURE_TO_OPEN_LOCAL_FILE = 1
 
 # An object to represent the attributes of a Linux user account.
 class Account:
-    # Construct an Account object using entries taken from /etc/passwd and /etc/shadow.
-    def __init__(self, passwdEntry, shadowEntry):
-        # Pull the needed fields from the entries. The field ordering in passwd and shadow are:
-        #    username:password:userID:groupID:gecos:homeDir:shell
-        #    username:password:lastchanged:minimum:maximum:warn:inactive:expire
-        [self.username, self.password, self.uid, self.gid, self.gecos, \
-            self.homeDir, self.shell] = passwdEntry.split(':')
-        self.password = shadowEntry.split(':')[1]  # Second field in /etc/shadow is password.
-
     # Construct an Account object from an /etc/passwd entry.
     def __init__(self, passwdEntry):
         # Pull the needed fields from the entries. The field ordering in passwd and shadow are:
@@ -64,15 +56,6 @@ class Account:
         #    username:password:lastchanged:minimum:maximum:warn:inactive:expire
         [self.username, self.password, self.uid, self.gid, self.gecos, \
             self.homeDir, self.shell] = passwdEntry.split(':')
-
-
-# Converts a list of lines to a dictionary keyed on the first thing before the delimiter.
-# def makeDictBySplitToFirstField(lines, delimiter):
-#     dict = {}
-#     for line in lines:
-#         head = line.split(delimiter, 1)[0]
-#         dict[head] = line
-#     return dict
 
 
 # Attempt to open a local text file and convert to a list of lines.
@@ -101,8 +84,8 @@ def getNonSystemUserData(target=None):
             print e
             exit(EXIT_CODE_FAILURE_TO_OPEN_LOCAL_FILE)
     else:  # If a remote target was given then open remote files.
-        passwdFile = subprocess.Popen(['ssh', target, 'sudo cat', '/etc/passwd'], stdout=subprocess.PIPE).stdout
-        shadowFile = subprocess.Popen(['ssh', target, 'sudo cat', '/etc/shadow'], stdout=subprocess.PIPE).stdout
+        passwdFile = subprocess.Popen(['ssh', target, 'cat', '/etc/passwd'], stdout=subprocess.PIPE).stdout
+        shadowFile = subprocess.Popen(['ssh', target, 'cat', '/etc/shadow'], stdout=subprocess.PIPE).stdout
 
     # Split text files into lists of lines.
     passwdEntries = passwdFile.read().splitlines()
@@ -126,59 +109,18 @@ def getNonSystemUserData(target=None):
     return users, userAccountDict
 
 
-# Read remote /etc/passwd and /etc/shadow files into dictionaries keyed by username.
-# def getRemoteUserEntries(target):
-#     # Read the /etc/passwd file into a list of lines, then convert it to a dictionary keyed by username.
-#     passwdFile = subprocess.Popen(['ssh', target, 'cat', '/etc/passwd'], stdout=subprocess.PIPE).stdout
-#     passwdEntries = passwdFile.read().splitlines()
-#     passwdDict = makeDictBySplitToFirstField(passwdEntries, ':')
-#
-#     # Read the /etc/shadow file into a list of lines, then convert it to a dictionary keyed by username.
-#     shadowFile = subprocess.Popen(['ssh', target, 'cat', '/etc/shadow'], stdout=subprocess.PIPE).stdout
-#     shadowEntries = shadowFile.read().splitlines()
-#     shadowDict = makeDictBySplitToFirstField(shadowEntries, ':')
-#
-#     return passwdDict, shadowDict
-#
-#
-# # Read local /etc/passwd and /etc/shadow files into dictionaries keyed by username.
-# def getLocalUserEntries():
-#     # Read the /etc/passwd file into a list of lines.
-#     with open('/etc/passwd', 'r') as localPasswdFile:
-#         passwdEntries = localPasswdFile.read().splitlines()
-#     passwdDict = makeDictBySplitToFirstField(passwdEntries, ':')
-#
-#     # Read the /etc/shadow file into a dictionary of lines keyed by username.
-#     with open('/etc/shadow', 'r') as shadowPasswdFile:
-#         shadowEntries = shadowPasswdFile.read().splitlines()
-#     shadowDict = makeDictBySplitToFirstField(shadowEntries, ':')
-#
-#     return passwdDict, shadowDict
-#
-#
-# # Convert passwd and shadow dictionaries into a list of Accounts while stripping out system users.
-# def getNonSystemAccounts(usernameList, passwdDict, shadowDict):
-#     accountList = []
-#     for username in usernameList:
-#         # Convert username to user Account instance.
-#         account = Account(passwdDict[username], shadowDict[username])
-#
-#         # Disregard system users.
-#         if account.uid < LOWEST_USER_ID:
-#             continue
-#         accountList.append(account)
-#
-#     return accountList
-
-
 # Create a new user account at a remote machine.
+# NOTE: User fields are copied as is with the exceptions:
+#       home directory is forced to be /home
+#       shell is forced to be /usr/sbin/nologin
 def addRemoteUser(target, account):
-    # Construct command.
+    # Construct and execute command to remotely add user.
     cmd = 'ssh -n ' + target + ' /usr/sbin/useradd -p "' + \
           account.password + '" -u ' + account.uid + ' -g ' + account.gid
     if account.gecos != "":
         cmd += '" -c "' + account.gecos + '"'
-    cmd += ' -M -s ' + account.shell + ' ' + account.username
+    cmd += ' -d "/home" -M -s "/usr/sbin/nologin" ' + account.username
+
     output = commands.getoutput(cmd)
     if output:
         print output
@@ -187,14 +129,15 @@ def addRemoteUser(target, account):
 # Delete a user account at a remote machine.
 def deleteRemoteUser(target, username):
     # Construct command.
-    cmd = 'ssh -n ' + target + ' /usr/sbin/deluser ' + username
+    cmd = 'ssh -n ' + target + ' /usr/sbin/deluser -quiet ' + username
+
     output = commands.getoutput(cmd)
     if output:
         print output
 
 
 # Change a user password at a remote machine
-def changeRemoteUserPassword(target, username, newPassword):
+def updateRemoteUserPassword(target, username, newPassword):
     # Construct command.
     cmd = "ssh -n " + target + " /usr/sbin/usermod -p '" + newPassword + "' " + username
 
@@ -203,28 +146,31 @@ def changeRemoteUserPassword(target, username, newPassword):
         print output
 
 
-# Turn a list of usernames into a string while limiting the possible length of the string.
+# Turn a list of usernames into a string but limit the possible length of the string.
+# Example: ["user1", "user2", "user3", "user4", "user5", "user6", "user7"]
+# Becomes: "user1 user2 user3 user4 user5 ...and 2 others."
 def usernameListToLimitedString(userList):
     global MOST_USERNAMES_TO_LIST
     returnString = ""
-    # Construct a string listing users without being too long.
+    # If there aren't too many usernames then list them all.
     if len(userList) <= MOST_USERNAMES_TO_LIST:
         for username in userList:
             returnString += username + ' '
-    # If there's lots of missing users then just list the first few
+    # If there's lots of usernames then just list the first few.
     else:
         for username in userList[:MOST_USERNAMES_TO_LIST]:
-            print username,
-            returnString += "... and " + \
-                str(len(userList) - MOST_USERNAMES_TO_LIST) + " others."
+            returnString += username + ' '
+        returnString += "...and " + \
+            str(len(userList) - MOST_USERNAMES_TO_LIST) + " others."
     return returnString
 
 
 def main():
     # Settings that will later be taken as command-line arguments.
-    # destAddress = 'root@192.168.20.45'
-    destAddress = 'pi@192.168.1.11'
+    destAddress = 'root@192.168.20.45'
+    # destAddress = 'pi@192.168.1.11'
     migrantUsersFilename = 'list_of_users.txt'
+    deleteUnlistedUsersFlag = True
 
     srcUsers, srcAccountDict = getNonSystemUserData()
     destUsers, destAccountDict = getNonSystemUserData(destAddress)
@@ -235,65 +181,57 @@ def main():
     # Any users at destination and not at source should be marked for deletion.
     doomedUsers = [user for user in destUsers if user not in srcUsers]
 
-    print doomedUsers
+    # If desired then also delete users who are not listed as migrants.
+    if deleteUnlistedUsersFlag:
+        unlistedUsers = [user for user in destUsers if user not in migrantUsers]
+        doomedUsers += [user for user in unlistedUsers if user not in doomedUsers]
+        print "Unlisted users: " + usernameListToLimitedString(unlistedUsers)  # DEBUG
 
-    # # Create lists of usernames and some counters.
-    # missingUsers, newUsers, changedUsers, doomedUsers = [], [], [], []
-    # newUserCount, changedUserCount, unchangedUserCount = 0, 0, 0
-    #
-    # # Analyse users from the given text file of usernames.
-    # for username in migrantUsers:
-    #     # If username not found at source machine add them to list of missing users.
-    #     if username not in srcPasswdDict:
-    #         missingUsers.append(username)
-    #
-    #     # If username found at source but not at destination then copy user over.
-    #     if username in srcPasswdDict and username not in destPasswdDict:
-    #         newUsers.append(username)
-    #
-    #     # If username not found at source but found at destination then delete user.
-    #     if username not in srcPasswdDict and username in destPasswdDict:
-    #         doomedUsers.append(username)
-    #
-    #     # If username found at source and destination then check if password has changed.
-    #     if username in srcPasswdDict and username in destPasswdDict:
-    #         srcPassword = srcShadowDict[username].split(':')[1]
-    #         destPassword = destShadowDict[username].split(':')[1]
-    #         if srcPassword != destPassword:
-    #             changedUsers.append(username)
-    #         else:
-    #             unchangedUserCount += 1
-    #
-    # # Migrate new users.
-    # if newUsers:
-    #     newAccounts = getNonSystemAccounts(newUsers, srcPasswdDict, srcShadowDict)
-    #     for account in newAccounts:
-    #         account.shell = "/usr/sbin/nologin"  # Disable shell for security sake.
-    #         account.homeDir = "/home"  # Give all users the same home directory.
-    #         print "Migrating new user: " + account.username
-    #         addRemoteUser(destAddress, account)
-    #
-    # # Delete users at destination if they have been marked for destruction.
-    # if doomedUsers:
-    #     for username in doomedUsers:
-    #         deleteRemoteUser(destAddress, username)
-    #
-    # # Update changed users.
-    # if changedUsers:
-    #     changedAccounts = getNonSystemAccounts(changedUsers, srcPasswdDict, srcShadowDict)
-    #     for account in changedAccounts:
-    #         print "Updating password for user: " + account.username
-    #         changeRemoteUserPassword(destAddress, account.username, account.password)
-    #
-    # # Warn of usernames that were not found on the source machine.
-    # if missingUsers:
-    #     print "WARNING: The following users were named in \"" + migrantUsersFilename + \
-    #           "\" but could not be found on the source machine:",
-    #     print usernameListToLimitedString(missingUsers)
-    #
-    # # Give a final accounting of the user migration results.
-    # print '\nUser outcomes: ' + str(len(newUsers)) + " migrated, " + str(len(changedUsers)) \
-    #     + " updated, " + str(unchangedUserCount) + " unchanged, " + str(len(doomedUsers)) + " deleted, " \
-    #     + str(len(missingUsers)) + " not found."
+    # Create lists of usernames and some counters.
+    missingUsers, newUsers, changedUsers = [], [], []
+    newUserCount, changedUserCount, unchangedUserCount = 0, 0, 0
+
+    # Analyze migrating users listed in the given text file.
+    for username in migrantUsers:
+        # If username not found at source machine add them to list of missing users.
+        if username not in srcUsers:
+            missingUsers.append(username)
+
+        # If username found at source but not at destination then copy user over.
+        if username in srcUsers and username not in destUsers:
+            newUsers.append(username)
+
+        # If username found at source and destination then check if password has changed.
+        if username in srcUsers and username in destUsers:
+            if srcAccountDict[username].password != destAccountDict[username].password:
+                changedUsers.append(username)
+            else:
+                unchangedUserCount += 1
+
+    # Migrate new users.
+    for username in newUsers:
+        print "Migrating new user: " + username  # DEBUG
+        addRemoteUser(destAddress, srcAccountDict[username])
+
+    # Delete users at destination if they have been marked for destruction.
+    for username in doomedUsers:
+        print "Deleting user: " + username  # DEBUG
+        deleteRemoteUser(destAddress, username)
+
+    # Update users who have changed their password.
+    for username in changedUsers:
+        print "Updating password for user: " + username  # DEBUG
+        updateRemoteUserPassword(destAddress, username, srcAccountDict[username].password)
+
+    # Warn of usernames that were not found on the source machine.
+    if missingUsers:
+        print "WARNING: The following users were named in \"" + migrantUsersFilename + \
+              "\" but could not be found on the source machine:",
+        print usernameListToLimitedString(missingUsers)
+
+    # Give a final accounting of the user migration results.
+    print "\nUser outcomes: " + str(len(newUsers)) + " migrated, " + str(len(changedUsers)) \
+        + " updated, " + str(unchangedUserCount) + " unchanged, " + str(len(doomedUsers)) + " deleted, " \
+        + str(len(missingUsers)) + " not found."
 
 main()
