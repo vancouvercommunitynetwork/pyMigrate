6f305477443f9dbeee0dbb2eae5ca62038f2f214
vcn <vcn@vcn.bc.ca>
2016-10-18 13:32:56 -0700

    Changed the usernames in makeDummies.sh and created the fixuid.sh script in order to fix all the uids for my test users to be the same numbers on both sides.

--- testing/makeDummies.sh
+++ testing/makeDummies.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo Creating dummies
-for i in $(seq 40000 40003)
+for i in $(seq 40001 40006)
 do
-    useradd -g 1105 -s "/usr/sbin/nologin" -c "John Smith" -d "/home" -M dummy$i
+    useradd -u $i -g 1105 -s "/usr/sbin/nologin" -c "John Smith" -d "/home" -M dummy$i
 done
 
 grep dummy /etc/passwd
