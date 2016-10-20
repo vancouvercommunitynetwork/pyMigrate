1c65f26df9a17c099d3da2e3b617809d0c964f9c
CodeBishop <srb@vcn.bc.ca>
2016-10-20 15:35:40 -0700

    Deleted various test files that are no longer used for testing.

--- testing/fixuid.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-for i in range(14147, 15100):
-    print "echo Fixing user " + str(i)
-    print "usermod -g 1105 -u " + str(i+40000) + " -c \"fake user\" -p nopass -d /home -s /usr/sbin/nologin test" + str(i)
--- testing/moophomerun.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# NOTE: The absurd 'moop' name is simply so that I can autocomplete on the first letter at the console.
-while true; do
-./homerun.sh "$@" -q
-sleep 1
-done
